ae871517e379cc368d93d83a9a8a2d626d157737
avirupchaki <avirup.chaki@gmail.com>
2021-05-08 23:35:13 +0530

    Added Controller.

---
+++ UserService/src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.user.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.user.entity.UserData;
+import com.user.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	private static final Logger log = LogManager.getLogger(UserController.class);
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/addUser")
+	public UserData addUser(@RequestBody UserData userData)
+	{
+		log.info("Inside addUser().");
+		return userService.addUser(userData);
+	}
+	
+	@GetMapping("/{id}")
+	public UserData getUserById(@PathVariable int id)
+	{
+		return userService.getUserById(id);
+	}
+}
